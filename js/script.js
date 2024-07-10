4957003a6bc2b012ff5a962f3cb9928934b25728
CROWNZWEBDEV <cergey13.96@gmail.com>
2024-07-10 23:12:03 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,182 @@
+var lnStickyNavigation;
+
+$(document).ready(function () {
+	applyHeader();
+	applyNavigation();
+	applyMailTo();
+	applyResize();
+	checkHash();
+	checkBrowser();
+
+	$('#currentAge').text(calculateAge(new Date('04/13/1996')));
+});
+
+function calculateAge(birthday) {
+	var ageDifMs = Date.now() - birthday.getTime();
+	var ageDate = new Date(ageDifMs);
+
+	return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+/* HEADER FUNCTIONS */
+
+function applyHeader() {
+	$('.jumbotron').css({ height: ($(window).height()) + 'px' });
+
+	lazyLoad($('.jumbotron'));
+}
+
+function lazyLoad(poContainer) {
+	var lstrCount = 4;
+	var lstrIndex = Math.floor(Math.random() * lstrCount) + 1;
+	var lstrSource = poContainer.attr('data-src') + lstrIndex + '.jpg';
+	var lstrPosition = poContainer.attr('data-position');
+
+	$('<img>').attr('src', lstrSource).load(function () {
+		poContainer.css('background-image', 'url("' + lstrSource + '")');
+		poContainer.css('background-position', lstrPosition);
+		poContainer.css('-ms-filter', '"progid:DXImageTransform.Microsoft.AlphaImageLoader(src=\'' + lstrSource + '\', sizingMethod=\'scale\')"');
+		poContainer.css('filter', 'progid:DXImageTransform.Microsoft.AlphaImageLoader(src=\'' + lstrSource + '\', sizingMethod=\'scale\'');
+	});
+}
+
+/* NAVIGATION FUNCTIONS */
+
+function applyNavigation() {
+	applyClickEvent();
+	applyNavigationFixForPhone();
+	applyScrollSpy();
+	applyStickyNavigation();
+}
+
+function applyClickEvent() {
+	$('a[href*=#]').on('click', function (e) {
+		e.preventDefault();
+
+		if ($($.attr(this, 'href')).length > 0) {
+			$('html, body').animate(
+				{
+					scrollTop: $($.attr(this, 'href')).offset().top
+				}, 400);
+		}
+		return false;
+	});
+}
+
+function applyNavigationFixForPhone() {
+	$('.navbar li a').click(function (event) {
+		$('.navbar-collapse').removeClass('in').addClass('collapse');
+	});
+}
+
+function applyScrollSpy() {
+	$('#navbar-example').on('activate.bs.scrollspy', function () {
+		window.location.hash = $('.nav .active a').attr('href').replace('#', '#/');
+	});
+}
+
+function applyStickyNavigation() {
+	lnStickyNavigation = $('.scroll-down').offset().top + 20;
+
+	$(window).on('scroll', function () {
+		stickyNavigation();
+	});
+
+	stickyNavigation();
+}
+
+function stickyNavigation() {
+	if ($(window).scrollTop() > lnStickyNavigation) {
+		$('body').addClass('fixed');
+	}
+	else {
+		$('body').removeClass('fixed');
+	}
+}
+
+/* MAILTO FUNCTION */
+
+function applyMailTo() {
+	$('a[href*=mailto]').on('click', function (e) {
+		var lstrEmail = $(this).attr('href').replace('mailto:', '');
+
+		lstrEmail = lstrEmail.split('').reverse().join('')
+
+		$(this).attr('href', 'mailto:' + lstrEmail);
+	});
+}
+
+/* RESIZE FUNCTION */
+
+function applyResize() {
+	$(window).on('resize', function () {
+		lnStickyNavigation = $('.scroll-down').offset().top + 20;
+
+		$('.jumbotron').css({ height: ($(window).height()) + 'px' });
+	});
+}
+
+/* HASH FUNCTION */
+
+function checkHash() {
+	lstrHash = window.location.hash.replace('#/', '#');
+
+	if ($('a[href=' + lstrHash + ']').length > 0) {
+		$('a[href=' + lstrHash + ']').trigger('click');
+	}
+}
+
+/* IE7- FALLBACK FUNCTIONS */
+
+function checkBrowser() {
+	var loBrowserVersion = getBrowserAndVersion();
+
+	if (loBrowserVersion.browser == 'Explorer' && loBrowserVersion.version < 8) {
+		$('#upgrade-dialog').modal({
+			backdrop: 'static',
+			keyboard: false
+		});
+	}
+}
+
+function getBrowserAndVersion() {
+	var laBrowserData = [{
+		string: navigator.userAgent,
+		subString: 'MSIE',
+		identity: 'Explorer',
+		versionSearch: 'MSIE'
+	}];
+
+	return {
+		browser: searchString(laBrowserData) || 'Modern Browser',
+		version: searchVersion(navigator.userAgent) || searchVersion(navigator.appVersion) || '0.0'
+	};
+}
+
+function searchString(paData) {
+	for (var i = 0; i < paData.length; i++) {
+		var lstrDataString = paData[i].string;
+		var lstrDataProp = paData[i].prop;
+
+		this.versionSearchString = paData[i].versionSearch || paData[i].identity;
+
+		if (lstrDataString) {
+			if (lstrDataString.indexOf(paData[i].subString) != -1) {
+				return paData[i].identity;
+			}
+		}
+		else if (lstrDataProp) {
+			return paData[i].identity;
+		}
+	}
+}
+
+function searchVersion(pstrDataString) {
+	var lnIndex = pstrDataString.indexOf(this.versionSearchString);
+
+	if (lnIndex == -1) {
+		return;
+	}
+
+	return parseFloat(pstrDataString.substring(lnIndex + this.versionSearchString.length + 1));
+}	
